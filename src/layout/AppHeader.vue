3cc62cd77e8eecee6cd909024810fafc2f789282
Patrick Croquet <patrick.croquet@wanadoo.fr>
2020-04-07 08:44:56 +0200

    premier dépôt

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -8,35 +8,35 @@
         <b-navbar-toggle  style="color:black;" target="nav-collapse">☰</b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-            <b-nav-item href="#">Accueil</b-nav-item>
-            <b-nav-item href="#">Blog</b-nav-item>
-            <b-nav-item href="#" disabled>Contact</b-nav-item>
-        </b-navbar-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Accueil</b-nav-item>
+                <b-nav-item href="#">Blog</b-nav-item>
+                <b-nav-item href="#" disabled>Contact</b-nav-item>
+            </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Recherche"></b-form-input>
-            <b-button variant="outline-success" size="sm" class="my-2 my-sm-0" type="submit">Recherche</b-button>
-            </b-nav-form>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Recherche"></b-form-input>
+                <b-button variant="outline-success" size="sm" class="my-2 my-sm-0" type="submit">Recherche</b-button>
+                </b-nav-form>
 
-            <b-nav-item-dropdown text="Langue" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
+                <b-nav-item-dropdown text="Langue" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+                </b-nav-item-dropdown>
 
-            <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-                <em>Utilisateur</em>
-            </template>
-            <b-dropdown-item href="#">Profil</b-dropdown-item>
-            <b-dropdown-item href="#">Déconnexion</b-dropdown-item>
-            </b-nav-item-dropdown>
-        </b-navbar-nav>
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                    <em>Utilisateur</em>
+                </template>
+                <b-dropdown-item href="#">Profil</b-dropdown-item>
+                <b-dropdown-item href="#">Déconnexion</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
         </b-collapse>
     </b-navbar>
 </div>    
---
+++ src/views/components/Carousel2.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        <b-carousel id="carousel1"
+                    controls
+                    indicators>
+            <!-- Text slides with image -->
+            <b-carousel-slide
+                caption="Les plus belles photos des Alpes"
+                text="Nulla vitae elit libero, a pharetra augue mollis interdum." 
+                img-src="img/mountains/montagne1.jpg">
+            </b-carousel-slide>
+            <b-carousel-slide
+                caption="Les bienfaits de la montagne l'Ã©tÃ©"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+                img-src="img/mountains/montagne2.jpg">
+            </b-carousel-slide>
+            <b-carousel-slide
+                caption="SÃ©jour ski"
+                text="Praesent commodo cursus magna, vel scelerisque nisl consectetur." 
+                img-src="img/mountains/montagne4.jpg">
+            </b-carousel-slide>
+        </b-carousel>
+    </section>
+</template>
+<script>
+import { BCarousel } from "bootstrap-vue/esm/components/carousel/carousel";
+import { BCarouselSlide } from "bootstrap-vue/esm/components/carousel/carousel-slide";
+
+export default {
+  components: {
+    BCarousel,
+    BCarouselSlide
+  }
+};
+</script>
+<style>
+</style>
