52a23b36c70e300bab056f11bc5294446738c7ad
heraudk <heraudk@users.noreply.github.com>
2020-12-10 17:21:23 +0100

    FIL ROUGE ARGON

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,46 +1,49 @@
+
 <template>
+
+
+
+
+
 <div>
-    <b-navbar toggleable="lg" type="black" variant="white">
+
+<img src="img/brand/bandeau test 11.png" class="img-fluid" alt="Responsive image">
+ 
+   
+
+
+    <b-navbar toggleable="lg" >
+        
+       
+
         <b-navbar-brand href="#">
-            <img src="img/brand/logo-3419889_960_720.png" style="width:150px; height:100px;" alt="Mon site">
+            <img src="img/brand/LOGOOO PAINT JAUNE PEIT- CopieBETA.png" style="width:232px; height:100px;" alt="Mon site">
         </b-navbar-brand>
 
         <b-navbar-toggle  style="color:black;" target="nav-collapse">☰</b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-                <b-nav-item href="#">Accueil</b-nav-item>
-                <b-nav-item href="#">Blog</b-nav-item>
-                <b-nav-item href="#" disabled>Contact</b-nav-item>
+                <b-nav-item href="#">Expertise</b-nav-item>
+                <b-nav-item href="#">Notre histoire</b-nav-item>
+                <b-nav-item href="#" disabled>Espace client</b-nav-item>
             </b-navbar-nav>
 
             <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Recherche"></b-form-input>
-                <b-button variant="outline-success" size="sm" class="my-2 my-sm-0" type="submit">Recherche</b-button>
-                </b-nav-form>
-
-                <b-nav-item-dropdown text="Langue" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template v-slot:button-content>
-                    <em>Utilisateur</em>
-                </template>
-                <b-dropdown-item href="#">Profil</b-dropdown-item>
-                <b-dropdown-item href="#">Déconnexion</b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-navbar-nav>
+           
         </b-collapse>
     </b-navbar>
-</div>    
+ 
+ 
+</div>
+
 </template>
+
+
+
+
+
+
 <script>
 import { BNavbar, BNavbarBrand, BNavbarToggle, BNavbarNav } from "bootstrap-vue/esm/components/navbar";
 import { BCollapse } from "bootstrap-vue/esm/components/collapse";
@@ -48,6 +51,10 @@ import { BNavItem, BNavItemDropdown, BNavForm } from "bootstrap-vue/esm/componen
 import { BDropdownItem } from "bootstrap-vue/esm/components/dropdown";
 import { BFormInput } from "bootstrap-vue/esm/components/form-input";
 import { BButton } from "bootstrap-vue/esm/components/button";
+
+
+
+
 
 export default {
   components: {
@@ -61,9 +68,24 @@ export default {
     BDropdownItem,
     BNavForm,
     BFormInput,
-    BButton 
+    BButton, 
+  
+    
   }
 };
+
+
+
 </script>
+
+
+
 <style>
+
+
+
+
+
 </style>
+
+
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,85 +1,73 @@
 <template>
     <!-- <section class="section section-lg section-shaped overflow-hidden my-0"> -->   
     <section>   
-                    <b-carousel id="carousel1"
-                                controls
-                                indicators>
-                        <!-- Text slides with image -->
-                        <b-carousel-slide
-                            caption="Les plus belles photos des Alpes"
-                            text="Nulla vitae elit libero, a pharetra augue mollis interdum." 
-                            img-src="img/mountains/montagne1.jpg">
-                        </b-carousel-slide>
-                        <b-carousel-slide
-                            caption="Les bienfaits de la montagne l'été"
-                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
-                            img-src="img/mountains/montagne2.jpg">
-                        </b-carousel-slide>
-                        <b-carousel-slide
-                            caption="Séjour ski"
-                            text="Praesent commodo cursus magna, vel scelerisque nisl consectetur." 
-                            img-src="img/mountains/montagne4.jpg">
-                        </b-carousel-slide>
-                    </b-carousel>
+                 
             <p></p>
 
             <b-container class="bv-example-row">                
-                <b-row>
-                    <b-col cols="12" md="4" class="text-center">
-                        <img v-lazy="'img/theme/gite.jpg'" alt="Raised circle image"
-                            class=" rounded-circle shadow-lg" style="width: 150px;">
-                        <h2>Gîte</h2>
-                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>                           
-                    </b-col>
-                    <b-col cols="12" md="4" class="text-center">
-                        <img v-lazy="'img/theme/randonnee.jpg'" alt="Raised circle image"
-                            class="img-fluid rounded-circle shadow-lg" style="width: 150px;">
-                        <h2>Randonnée</h2>
-                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-                        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>        
-                    </b-col>
-                    <b-col cols="12" md="4" class="text-center">
-                        <img v-lazy="'img/theme/ski.jpg'" alt="Raised circle image"
-                            class="img-fluid rounded-circle shadow-lg" style="width: 150px;">
-                        <h2>ski</h2>
-                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>                       
-                    </b-col>              
-                </b-row>
-
-                <p>{{ message }}</p>
+                                
+                
+               
 
                 <hr class="featurette-divider">
                 <div class="row featurette">
+
                 <div class="col-md-7">
-                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading"> <span class="text-muted"></span></h2>
+                    <p class="lead">Notre agence web accompagne nos clients dans la <font>création 
+              de site internet</font> ou la <font>refonte de votre site</font> web:
+              - gestion de projet
+              - audit technique
+              - l'identité visuelle à votre image
+              - design & ergonomie
+              - conception graphique
+              - expérience client
+             - développement web sur-mesure
+              - référencement naturel
+              - référencement payant
+              - média sociaux </p>
                 </div>
+
                 <div class="col-md-5">
-                    <img src="img/mountains/montagne3.jpg" width="100%" height="100%" alt="">
+                    <img src="img/brand/unnamed (1).jpg" width="100%" height="100%" alt="">
                 </div>
                 </div>
+
+
+
+
                 <hr class="featurette-divider">        
                 <div class="row featurette">
                 <div class="col-md-7 order-md-2">
-                    <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading"> <span class="text-muted"></span></h2>
+                    <p class="lead">
+              <br> vous confiez la création de votre site internet à une agence web ?
+              Notre agence conseil est <font>spécialiste dans la création de site web</font> afin 
+              de répondre à tous vos besoins et d'élaborer une véritable <font>stratégie 
+              digitale sur-mesure</font>.<br>
+  
+              <br>Construire un site web requiert des compétences en graphisme
+              pour la <font>réalisation de votre logo</font> et design des pages du site, 
+              webmarketing pour avoir un site web performant et <font>générer de 
+              nouveaux clients</font> et enfin en référencement naturel et payant pour être 
+              visible sur Google et développer votre acquisition de trafic. <br> 
+              
+               </p>
+
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <img src="img/mountains/montagne5.jpg" width="100%" height="100%" alt="">
+                    <img src="img/brand/mathilde.jpg" width="100%" height="100%" alt="">
                 </div>
-                </div>        
-                <hr class="featurette-divider">            
-                <div class="row featurette">
-                <div class="col-md-7">
-                    <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                </div>
-                <div class="col-md-5">
-                    <img src="img/mountains/vtt.jpg" width="100%" height="100%" alt="">
-                </div>
-                </div>
+                </div>       
+
+
+
+
+
+
+ <div class="video"> <iframe width="560" height="315" src="https://www.youtube.com/embed/56NfqCe9Uc4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>             </div>
+             
+                
             </b-container>
     </section>  
 </template>
@@ -106,3 +94,18 @@ export default {
   }
 };
 </script>
+
+
+
+
+<style>
+iframe
+
+    {
+           max-width: 580px;
+           height: 380px;
+         }
+
+
+
+</style>
